3abd9b66e84fac4dee85b8e59b7bc6cf7148757f
pashaarshad <arshadpashaintern@gmail.com>
2026-08-30 19:59:33 +0530

    workign-backend arsahd

--- Backend/parttime.com/thinkphp/library/think/exception/Handle.php
+++ Backend/parttime.com/thinkphp/library/think/exception/Handle.php
@@ -180,9 +180,13 @@ class Handle
         $data['echo'] = ob_get_clean();
 
         ob_start();
-        extract($data);
-        // var_dump(Container::get('app')->config('exception_tmpl'));
-        include Container::get('app')->config('exception_tmpl');
+        $tmpl = Container::get('app')->config('exception_tmpl');
+        if (empty($tmpl) || !is_file($tmpl)) {
+            echo "<h1>Exception Occurred:</h1>";
+            echo "<pre>" . $exception->getMessage() . "\nFile: " . $exception->getFile() . " on line " . $exception->getLine() . "\n\n" . $exception->getTraceAsString() . "</pre>";
+        } else {
+            include $tmpl;
+        }
 
         // 获取并清空缓存
         $content  = ob_get_clean();
