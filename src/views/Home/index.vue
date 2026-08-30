20bce1c0348b08e7625dd9b5efcf55586452fc92
pashaarshad <arshadpashaintern@gmail.com>
2026-08-30 19:12:22 +0530

    ok

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -268,7 +268,7 @@ const onMenuClick = (path) => {
       border-radius: 7px;
       overflow: hidden;
       box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-      height: 164px;
+      height: 189px;
     }
 
     .banner {
