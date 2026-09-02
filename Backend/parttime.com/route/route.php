9585d7d6271bf2968ffaa78fc5fad9ba53282f39
pashaarshad <arshadpashaintern@gmail.com>
2026-09-02 08:36:46 +0530

    fix: add route mapping for admin/owe9j2 to resolve 404 controller not exists error on custom login entry

--- Backend/parttime.com/route/route.php
+++ Backend/parttime.com/route/route.php
@@ -2,5 +2,9 @@
 use think\facade\Route;
 
 Route::rule('owe9j2/login', 'admin/login/index');
+Route::rule('owe9j2$', 'admin/login/index');
 Route::rule('owe9j2', 'admin/login/index');
+Route::rule('admin/owe9j2$', 'admin/login/index');
+Route::rule('admin/owe9j2', 'admin/login/index');
+Route::rule('admin/login$', 'admin/login/index');
 Route::rule('admin/login', 'admin/login/index');
